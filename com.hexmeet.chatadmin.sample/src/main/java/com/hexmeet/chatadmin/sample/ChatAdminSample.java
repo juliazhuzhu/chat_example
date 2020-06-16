d7e1c5314df4622f46b21659ad9e552dcb8d3d8d
xiaoye <3144605@qq.com>
2020-06-16 14:53:08 +0800

    first submit

---
+++ com.hexmeet.chatadmin.sample/src/main/java/com/hexmeet/chatadmin/sample/ChatAdminSample.java
@@ -0,0 +1,31 @@
+package com.hexmeet.chatadmin.sample;
+
+import com.hexmeet.chat.admin.ChatAdminClient;
+
+public class ChatAdminSample {
+	
+	
+	
+	
+public static void main(String[] args) throws InterruptedException {
+    	
+    	ChatAdminClient admin = ChatAdminClient.getDefaultInstance();
+    	admin.setHost("127.0.0.1");
+    	admin.setPort(6061);
+    	admin.setUser("hexmeetim");
+    	admin.setPassword("2ghlmcl@#$");
+    	//admin.setLogPath("/Users/zhuyiye/Downloads/MyLogFile.log");
+    	//admin.createChatGroup(topic_name, pri_comments);
+    	ChatAdminSampleEventHandler event_handler = new ChatAdminSampleEventHandler();
+    	admin.setEventNotifier(event_handler);
+    	Thread admin_thread = new Thread(admin);
+    	admin_thread.start();
+    	//admin.login();
+    	//admin.stop();
+    	admin_thread.join();
+        
+    	System.out.println("app exited...");
+   
+    }
+
+}
